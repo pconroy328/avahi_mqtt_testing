e0289da28069b8a86cfeaa8fe9c1f6f701e236b5
Patrick Conroy <patrick@conroy-family.net>
2019-07-31 03:16:31 -0600

    passing in service and domain

--- main.c
+++ main.c
@@ -9,10 +9,19 @@
  */
 
 #include <libmqttrv.h>
+#include "uthash/utlist.h"
+
 
 int main(int argc, char*argv[]) 
 {
-    MQTTBrokers_t  *list = MQTT_FindAllBrokers();
+    MQTTBrokers_t  *head = MQTT_FindAllBrokers( "_mqtt._tcp", NULL );
+    MQTTBrokers_t  *element1, *element2;
+    
+    LL_FOREACH_SAFE( head, element1, element2) {
+        printf( "Found service at host [%s], address [%s], port [%d], description [%s]\n", element1->hostName, element1->address, element1->portNumber, element1->description );
+    }
+    
+    MQTT_CleanUp_After_Find();
 }
 
 
