68ccc60e541a5b7e59f6236a9af94790272388b6
Patrick Conroy <patrick@conroy-family.net>
2019-08-01 15:32:06 -0600

    added new Connect Call

--- main.c
+++ main.c
@@ -10,10 +10,18 @@
 
 #include <libmqttrv.h>
 #include "uthash/utlist.h"
+#include <mosquitto.h>
 
+
+extern  int             MQTT_Connect( const char *brokerHostName, struct mosquitto **aMosquittoInstance, const int anyBrokerAllowed );
 
 int main(int argc, char*argv[]) 
 {
+    struct mosquitto *aMosquittoInstance;
+    MQTT_Connect( "mqttrv", &aMosquittoInstance, 1 );
+    
+    
+    
     MQTTBrokers_t  *head = MQTT_FindAllBrokers( "_mqtt._tcp", NULL );
     MQTTBrokers_t  *element1, *element2;
     
@@ -23,7 +31,7 @@ int main(int argc, char*argv[])
     
     MQTT_CleanUp_After_Find();
     
-    *head = MQTT_FindABroker( "mqttrv", "_mqtt._tcp", NULL );
+    head = MQTT_FindABroker( "mqttrv", "_mqtt._tcp", NULL );
            
     LL_FOREACH_SAFE( head, element1, element2) {
         printf( "Found service at host [%s], address [%s], port [%d], description [%s]\n", element1->hostName, element1->address, element1->portNumber, element1->description );
